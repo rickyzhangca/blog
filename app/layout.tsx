1fbf2725cc63b9b23aad2b04c7d22650e5693cd6
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2025-07-22 00:38:32 -0400

    lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import { Analytics } from '@vercel/analytics/next';
 import { cn } from '@/lib/utils';
 import { Footer } from './footer';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
